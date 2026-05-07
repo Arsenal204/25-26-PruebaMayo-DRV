97fda443c76ad6aa55acc1f62fbf913530ae70e6
Diego Robles <diegorobles031204@gmail.com>
2026-05-07 18:07:30 +0200

    update del database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ docstring del módulo
 '''
 
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative imports declarative_base
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
