c57c3c38102ac43069b3d7a9b788bac493c34de5
Diego Robles <diegorobles031204@gmail.com>
2026-05-07 18:02:19 +0200

    update del database.py

--- app/database.py
+++ app/database.py
@@ -1,3 +1,7 @@
+'''
+docstring del módulo 
+'''
+
 from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
@@ -8,6 +12,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./inventory.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SESSION_LOCAL = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
