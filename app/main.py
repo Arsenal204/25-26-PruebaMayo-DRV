c0284b78585566f41c01e48c07d9521f303a4bcb
Diego Robles <diegorobles031204@gmail.com>
2026-05-07 17:38:57 +0200

    update main y prueba de actions

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ app.include_router(items.router)
 # Código que se añade
 @app.get("/status")
 def version():
-    return {"status": "Robles Vidal, Diego - v.xx"}
+    return {"status": "Robles Vidal, Diego - v.Xxx"}
